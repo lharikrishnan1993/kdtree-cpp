d0c1641a4786a7c454038c44d828d7c33f9026e3
Harikrishnan Lakshmanan <hlakshmanan@wpi.edu>
2017-01-07 21:27:43 -0500

    Insertion strategy created

--- src/main.cpp
+++ src/main.cpp
@@ -40,77 +40,83 @@ node <double> *grow_kdtree(kdtree <double> *tree, std::vector <double> &data)
     return head;
 }
 
-int get_split_axis(std::vector <std::vector<double> > *dataset)
+void get_split_axis(int *axis, std::vector<std::vector<double>> *dataset)
 {
     std::map<double,int> ranges;
     std::vector <double> temp_vector;
     ranges.clear();
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < (*dataset)[0].size(); i++)
     {
         temp_vector.clear();
         for (int j = 0; j < dataset->size(); j++)
             temp_vector.push_back((*dataset)[j][i]);
         ranges[*std::max_element(temp_vector.begin(), temp_vector.end()) - *std::min_element(temp_vector.begin(), temp_vector.end())] = i;
     }
-    return ranges.rbegin()->second;
+    *axis = ranges.rbegin()->second;
 }
 
-node <double> *build_tree(kdtree <double> &tree, std::vector <std::vector<double> > *dataset)
+class median_data
+{
+    public:
+        std::vector<double> data;
+        std::vector<std::vector <double>> data_left;
+        std::vector<std::vector <double>> data_right;
+};
+
+void get_median(median_data *median_details, std::vector<std::vector<double>> *dataset)
+{
+    int axis = 0;
+    get_split_axis(&axis, dataset);
+    median_details->data_left.clear();
+    median_details->data_right.clear();
+    median_details->data.clear();
+
+    std::sort(dataset->begin(), dataset->end(), [axis](const std::vector <double> &a, const std::vector <double> &b)
+        { return  a.at(axis) < b.at(axis); });
+
+    int median_index = 0;
+    if (dataset->size()%2 == 0) median_index = dataset->size()/2 - 1;
+    else median_index = dataset->size()/2;
+
+    std::vector<std::vector<double>>::iterator it;
+    int i = 0;
+
+    for(it = dataset->begin(); it < dataset->end(); it++, i++ )
+    {
+        if (i < median_index) median_details->data_left.push_back(*it);
+        else if (i > median_index) median_details->data_right.push_back(*it);
+        else median_details->data = *it;
+    }
+}
+
+node <double> *build_tree(kdtree <double> &tree, std::vector<std::vector<double>> *dataset)
 {
     static node <double> *root = nullptr;
 
-    if (dataset->size() == 0) return root;
+    if (dataset->size() == 0) return root
 
-// Once done with the reconstruction, the following block is not needed
-    for (int i = 0; i < dataset->size(); i++)
-    {
-        root = grow_kdtree(&tree, (*dataset)[i]);
-    }
+    median_data *details = new median_data;
+    get_median(details, dataset);
+
+    root = grow_kdtree(&tree, details->data);
+    build_tree(tree, &details->data_left);
+    build_tree(tree, &details->data_right);
 
     return root;
 }
 
 int main()
 {
-    std::vector <std::vector <double>> dataset;
+    std::vector<std::vector<double>> whole_data;
     kdtree <double> tree;
     node <double> *nn;
     node <double> *root;
     std::vector <double> data;
 
-    data = {50,0};
-//    std::cout<<data.size()<<std::endl;
-    dataset.push_back(data);
-//    std::cout<<dataset.size()<<std::endl;
-    data.clear();
+    whole_data = {{50,0}, {25,1}, {75,1}, {60,2}, {100,5}, {10,6}, {49,3}};
 
-    data = {25,1};
-    dataset.push_back(data);
-    data.clear();
-
-    data = {75,1};
-    dataset.push_back(data);
-    data.clear();
-
-    data = {60,2};
-    dataset.push_back(data);
-    data.clear();
-
-    data = {100,5};
-    dataset.push_back(data);
-    data.clear();
-
-    data = {200,6};
-    dataset.push_back(data);
-    data.clear();
-
-    data = {49,3};
-    dataset.push_back(data);
-    data.clear();
-
-    std::cout<<get_split_axis(&dataset)<<std::endl;
-    root = build_tree(tree, &dataset);
+    root = build_tree(tree, &whole_data);
 
     std::cout<<std::endl<<"Printing Tree..."<<std::endl;
     tree.print_tree(root);
