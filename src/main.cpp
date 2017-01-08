732e3d0d186a544e06d0506288ca544e43d69f32
Harikrishnan Lakshmanan <hlakshmanan@wpi.edu>
2017-01-07 19:42:56 -0500

    split axis function

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@
 */
 
 #include "kdtree_utils.cpp"
-#include <typeinfo>
+#include <algorithm>
+#include <map>
 
 node <double> *grow_kdtree(kdtree <double> *tree, std::vector <double> &data)
 {
@@ -39,13 +40,34 @@ node <double> *grow_kdtree(kdtree <double> *tree, std::vector <double> &data)
     return head;
 }
 
-node <double> *build_tree(kdtree <double> &tree, std::vector <std::vector<double>> &dataset)
+int get_split_axis(std::vector <std::vector<double> > *dataset)
 {
-    node <double> *root;
-    for (int i = 0; i < dataset.size(); i++ )
+    std::map<double,int> ranges;
+    std::vector <double> temp_vector;
+    ranges.clear();
+
+    for (int i = 0; i < 2; i++)
     {
-        root = grow_kdtree(&tree, dataset[i]);
+        temp_vector.clear();
+        for (int j = 0; j < dataset->size(); j++)
+            temp_vector.push_back((*dataset)[j][i]);
+        ranges[*std::max_element(temp_vector.begin(), temp_vector.end()) - *std::min_element(temp_vector.begin(), temp_vector.end())] = i;
     }
+    return ranges.rbegin()->second;
+}
+
+node <double> *build_tree(kdtree <double> &tree, std::vector <std::vector<double> > *dataset)
+{
+    static node <double> *root = nullptr;
+
+    if (dataset->size() == 0) return root;
+
+// Once done with the reconstruction, the following block is not needed
+    for (int i = 0; i < dataset->size(); i++)
+    {
+        root = grow_kdtree(&tree, (*dataset)[i]);
+    }
+
     return root;
 }
 
@@ -55,15 +77,46 @@ int main()
     kdtree <double> tree;
     node <double> *nn;
     node <double> *root;
+    std::vector <double> data;
 
-    dataset = {{50,0}, {25,1}, {75,1}, {60,2}, {100,5}, {10,6}, {49,3}};
-    root = build_tree(tree, dataset);
+    data = {50,0};
+//    std::cout<<data.size()<<std::endl;
+    dataset.push_back(data);
+//    std::cout<<dataset.size()<<std::endl;
+    data.clear();
+
+    data = {25,1};
+    dataset.push_back(data);
+    data.clear();
+
+    data = {75,1};
+    dataset.push_back(data);
+    data.clear();
+
+    data = {60,2};
+    dataset.push_back(data);
+    data.clear();
+
+    data = {100,5};
+    dataset.push_back(data);
+    data.clear();
+
+    data = {200,6};
+    dataset.push_back(data);
+    data.clear();
+
+    data = {49,3};
+    dataset.push_back(data);
+    data.clear();
+
+    std::cout<<get_split_axis(&dataset)<<std::endl;
+    root = build_tree(tree, &dataset);
 
     std::cout<<std::endl<<"Printing Tree..."<<std::endl;
     tree.print_tree(root);
 
     std::cout<<std::endl<<"Searching Tree..."<<std::endl;
-    std::vector <double> data = {51,5};
+    data = {51,5};
     std::vector <double> dat = tree.search_kdtree(data);
     std::cout<<std::endl<<"Nearest Neigbor: "<<dat[0]<<" "<<dat[1]<<std::endl;
 //    nn->check_point();
